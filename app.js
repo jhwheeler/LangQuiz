ee4c1a19218390d2c6998d4062535aeb3308549d
Alacritas <jhwheeler@gmail.com>
2017-01-16 20:12:01 +0200

    renderActions and eventHandlers

--- app.js
+++ app.js
@@ -1,79 +1,100 @@
 'use strict';
 
 var state = {
-    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    choices: ["a", "b", "c", "d"],
-    score: [],
+    currentQuestion: "",
+    score: 0,
     message: ["Congratulations!", "Well done!", "Not bad", "Could be better", "You've got some work to do..."]
 };
 
-var questions = {
-    "What is a phoneme?": ["A perceptually distinct set of sounds, perceived as equivalent, that distinguishes one word from another in a given language.", "A sound, of course! Is this a trick question?", "A letter in a given language that corresponds to a certain sound.", "The smallest meaning-bearing unit in a given language."],
-    "What is a morpheme?": ["The smallest meaningful unit in language.", "Another name for syllable, or collection of sounds with one nucleus, usually a vowel.", "The root of a word, such as 'read-' in the word 'reader'.", "A prefix or suffix that can be added to the beginning or ending of a word to make a new word."],
-    "Which sentence is in VSO word order?": ["Gather ye rosebuds while ye may.", "If music be the food of love, play on.", "Hope is a good breakfast, but it is a bad supper.", "With this ring, I thee wed."],
-    "A linguist is someone who:": ["Systematically studies the structure of language as a whole and/or specific languages.", "Speaks and writes properly, according to the accepted conventions of the given language.", "Speaks many languages; technically, at least 3."]
-};
-
-var templates = {
-    questionTemplate: (
-        '<div class="question js-question">' +
-            '<span class="question-number js-question-number"></span>' + 
-            '<span class="question-text js-question-text"></span>' +
-        '</div>'
-    ),
-    answerTemplate: (
-        '<div class="answer js-answer">' +
-            '<span class="js-answer-choice"></span>' +
-            '<span class="js-answer-text"></span>' +
-        '</div>'
-    ),
-    progressTemplate: (
-        '<div class="progress js-progress">' +
-            '<span class="js-progress-number"></span>' +
-            '<span> out of 5 questions complete</span>' +
-        '</div>'
-    ),
-    scoreTemplate: (
-        '<div class="score js-score">' +
-            '<span class="js-correct-score"></span>' +
-            '<span> correct | </span>' +
-            '<span class="js-incorrect-score"></span>' +
-            '<span>incorrect</span>' +
-        '</div>'
-    ),
-    resultsTemplate: (
-        '<div class="results js-results">' +
-            '<span class="js-message"></span>' +
-            '<span> You got </span>' +
-            '<span class="js-final-score"></span>' +
-            '<span> correct out of 5.</span>' +
-        '</div>'
-    )
-};
-
-//state modification functions
-
-var stateHelper = {
-    getQuestion: function(questions) {
-        var keys = Object.keys(questions);
-        var currentQuestion = keys[keys.length * Math.random() << 0];
-        console.log(currentQuestion);
-        return currentQuestion;
+var questions = [
+    {
+        question: "1. What is a phoneme?",
+        choices: [
+            "A perceptually distinct set of sounds, perceived as equivalent, that distinguishes one word from another in a given language.", 
+            "A sound, of course! Is this a trick question?",
+            "A letter in a given language that corresponds to a certain sound.",
+            "The smallest meaning-bearing unit in a given language."
+        ],
+        correctAnswer: 0,
+        chosenAnswer: null
     },
-    sortAnswers: function(state, questions) {
-        //sort answers at random
-        var keys = Object.keys(questions)
-        
+    {
+        question: "2. What is a morpheme?",
+        choices: [
+            "The smallest meaningful unit in language.", 
+            "Another name for syllable, or collection of sounds with one nucleus, usually a vowel.", 
+            "The root of a word, such as 'read-' in the word 'reader'.", 
+            "A prefix or suffix that can be added to the beginning or ending of a word to make a new word."
+        ],
+        correctAnswer: 0,
+        chosenAnswer: null
+    },
+    {
+        question: "3. Which sentence is in VSO word order?",
+        choices: [
+            "Gather ye rosebuds while ye may.", 
+            "If music be the food of love, play on.", 
+            "Hope is a good breakfast, but it is a bad supper.", 
+            "With this ring, I thee wed."
+        ],
+        questionNumber: 2,
+        correctAnswer: 0,
+        chosenAnswer: null
+    },
+    {
+        question: "4. A linguist is someone who...",
+        choices: [
+            "Systematically studies the structure of language as a whole and/or specific languages.", 
+            "Speaks and writes properly, according to the accepted conventions of the given language.", 
+            "Speaks many languages; technically, at least 3.",
+            "Dictates how language should be spoken by the general public."
+        ],
+        questionNumber: 3,
+        correctAnswer: 0,
+        chosenAnswer: null
+    }
+];
 
-        //assign them to choices array
+
+var renderActions = {
+    renderQuestion: function(state, questions, element) {
+        var q = state.currentQuestion;
+        element.find('.js-question-text').text(q.question);
+        return element;
+    },
+    renderChoices: function(state) {
+        c = questions[id].choices;
+        c.sort(function() { return .5 - Math.random(); });
+        for (var i = 0; i < questions.length; i++) {
+            console.log(questions[i].question);
+            console.log(questions[i].choices);
+        }
     }
 }
 
+var eventActions = {
+    nextButtonHandler: function(state) {
+        nextButton.submit(function(event) {
+            return state.currentQuestion++;
+        }
+    }
+}
 
+//in state, keep track of current question
+//when click next, call renderQuestion()
+//nextButtonHandler() > +1 to the currentQuestion in state
+
+
+/*show correct answer
+ questions[0].choices[questions[0].correctAnswer]
+*/
 
 
 $(function() {
-    stateHelper.getQuestion(questions);
+    var nextButton = '.js-nextButton';
+    var quizAnswers = '.js-answers';
+    renderActions.renderQuestion(state, questions, quizAnswers));
+    renderActions.renderChoices(state);
 });
 
 
@@ -96,11 +117,43 @@ $(function() {
 // 8. Once all questions are answered, display resultsTemplate
 //
 //
-// getQuestion()
-// loadQuestion()
+// renderQuestion()
+// renderChoices()
 // updateProgress()
 // displayCorrectAnswer()
 // updateScore()
 // displayResults()
 //
 //
+//1. Just question-answer part (w/o showing correct answer or score)
+//2. Add scores
+//3. Add where they are
+//4. Add showing the correct answer
+//5. Add the final results
+
+
+
+
+
+
+
+
+
+
+
+/*
+var stateHelper = {
+    getQuestion: function(questions) {
+        var keys = Object.keys(questions);
+        var currentQuestion = keys[keys.length * Math.random() << 0];
+        return currentQuestion;
+    },
+    sortAnswers: function(state, questions) {
+        //sort answers at random
+        var keys = Object.keys(questions)
+        
+
+        //assign them to choices array
+    }
+}
+*/
